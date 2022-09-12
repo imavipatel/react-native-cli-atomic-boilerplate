9154696efa7126cf314fc25cffa0ad094c52737b
Matt Oracz <contact@mattoracz.com>
2022-09-12 14:28:10 +0200

    Updated molecule/Input tests

--- src/components/molecules/Input/Input.test.tsx
+++ src/components/molecules/Input/Input.test.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
-import { Input } from './Input';
+import React, { createRef } from 'react';
+import { act, fireEvent, render, waitFor } from '@testing-library/react-native';
+import { Input, InputReference } from './Input';
 
 describe('TextInput', () => {
   beforeEach(() => {
@@ -39,5 +39,71 @@ describe('TextInput', () => {
     const { getByText } = render(component);
     const input = getByText(label);
     expect(input).toBeDefined();
+  });
+
+  it('should contain correct props', async () => {
+    const inputRef = createRef<InputReference>();
+    const testID = 'input';
+    const label = 'input placeholder';
+    const keyboardType = 'email-address';
+    const secureTextEntry = true;
+
+    const { getByTestId } = render(
+      <Input
+        ref={inputRef}
+        testID={testID}
+        label={label}
+        keyboardType={keyboardType}
+        secureTextEntry={secureTextEntry}
+      />,
+    );
+    const textInput = getByTestId(testID);
+    expect(textInput.props.keyboardType).toEqual(keyboardType);
+    expect(textInput.props.secureTextEntry).toEqual(secureTextEntry);
+    expect(textInput.props.testID).toEqual(testID);
+  });
+
+  it('should be able to setValue by reference', async () => {
+    const inputRef = createRef<InputReference>();
+
+    const testID = 'input';
+    render(<Input ref={inputRef} testID={testID} />);
+
+    expect(inputRef.current).not.toBeNull();
+    const testValue = 'Test value';
+    act(() => {
+      inputRef.current?.setValue(testValue);
+    });
+    expect(inputRef.current?.getValue()).toBe(testValue);
+  });
+
+  it('should be able to getValue by reference', async () => {
+    const inputRef = createRef<InputReference>();
+    const testID = 'input';
+    const { getByTestId } = render(<Input ref={inputRef} testID={testID} />);
+    const textInput = getByTestId(testID);
+    const testValue = 'Test value';
+    fireEvent.changeText(textInput, testValue);
+    await waitFor(() => expect(inputRef.current?.getValue()).toBe(testValue));
+  });
+
+  it('should be able to focus by reference', async () => {
+    const inputRef = createRef<InputReference>();
+    const testID = 'input';
+
+    render(<Input ref={inputRef} testID={testID} />);
+    inputRef.current?.focus();
+    expect(inputRef.current?.isFocused).toBeDefined();
+    expect(inputRef.current?.isFocused()).toEqual(undefined);
+  });
+
+  it('should be able to blur by reference', async () => {
+    const inputRef = createRef<InputReference>();
+    const testID = 'input';
+
+    render(<Input ref={inputRef} testID={testID} />);
+    inputRef.current?.blur();
+    expect(inputRef.current?.isBlured).toBeDefined();
+    expect(inputRef.current?.isBlured()).toBeTruthy();
   });
 });
